bb5d85cc84f13b4e3f88bf04d17cfd7b8a23285a
Marcin Majewski <majewskimarcin@outlook.com>
2016-11-12 17:31:47 +0100

    * Adding login form

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,12 +2,18 @@
 var app = angular.module('myApp', ['ui.router', 'xeditable']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/login');
 
     $stateProvider.state('home', {
         url: '/home',
         templateUrl: 'views/pages/home.html',
         controller: 'homeController'
+    });
+
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'views/login/login.html',
+        controller: 'loginController'
     })
 }]);
 
---
+++ src/main/resources/static/views/login/login.js
@@ -0,0 +1,5 @@
+angular.module('myApp').controller('loginController', ['$scope', 'ItemService', '$filter',
+    function ($scope, ItemService, $filter) {
+
+    }
+]);
